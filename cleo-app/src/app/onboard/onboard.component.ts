2d6f2f3646b909d2d3a30c3ec3677f9ccb8e0b3e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 09:14:30 +0200

    onboard: drafted template

--- cleo-app/src/app/onboard/onboard.component.ts
+++ cleo-app/src/app/onboard/onboard.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-onboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './onboard.component.html',
   styleUrls: ['./onboard.component.scss']
 })
 export class OnboardComponent {
-
 }
