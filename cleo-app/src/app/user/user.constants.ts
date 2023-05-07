7887935ed5e6aee28bcdf2c3a25c32108ccc6185
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 13:00:51 +0200

    user: implemented authorized data stream

---
+++ cleo-app/src/app/user/user.constants.ts
@@ -0,0 +1,6 @@
+const CLEO_API_URL = 'http://localhost:8240/'
+const CLEO_API_AUTH_URL = CLEO_API_URL + 'auth/';
+export const CLEO_API_PROTECTED_URL = CLEO_API_URL + 'protected/';
+export const CLEO_API_LOGIN_URL = CLEO_API_AUTH_URL + 'login/';
+export const CLEO_API_LOGOUT_URL = CLEO_API_AUTH_URL + 'logout/';
+export const CLEO_API_REGISTER_URL = CLEO_API_AUTH_URL + 'register/';
--- cleo-app/src/app/user/user.service.ts
+++ cleo-app/src/app/user/user.service.ts
@@ -1,9 +1,18 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpService} from "../http/http.service";
+import {map, Observable} from "rxjs";
+import {CLEO_API_PROTECTED_URL} from "./user.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private http = inject(HttpService);
+
+  public readonly authorized$: Observable<boolean> =
+    this.http.getRequest$(CLEO_API_PROTECTED_URL).pipe(
+      map((response) => {
+        return response.ok;
+      })
+    );
 }
