da1478ec2d8164e9cedb30ff29195f2afec0e2f9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 09:48:06 +0200

    sign-up: drafted component template

--- cleo-app/src/app/sign-up/sign-up.component.ts
+++ cleo-app/src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {BehaviorSubject} from "rxjs";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators}
+  from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  private formBuilder = inject(FormBuilder);
+  public error = new BehaviorSubject<string>('');
+  public signUpForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.minLength(4)],
+    password: ['', Validators.minLength(8)],
+  })
 
+  public submit() {
+    if (this.signUpForm.get('username')?.invalid
+      && this.signUpForm.get('password')?.invalid) {
+      this.error.next('Invalid username and password.');
+      return;
+    }
+
+    if (this.signUpForm.get('username')?.invalid) {
+      this.error.next('Invalid username.');
+      return;
+    }
+
+    if (this.signUpForm.get('password')?.invalid) {
+      this.error.next('Invalid password.');
+      return;
+    }
+  }
 }
