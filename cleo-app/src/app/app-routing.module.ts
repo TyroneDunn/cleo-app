9fef829f5830c06cda070a933e615bd4ebc9a874
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 09:02:13 +0200

    sign-in: added route

--- cleo-app/src/app/app-routing.module.ts
+++ cleo-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {OnboardComponent} from "./onboard/onboard.component";
 import {AboutComponent} from "./about/about.component";
+import {SignInComponent} from "./sign-in/sign-in.component";
 
 const routes: Routes = [
   {path: '', component: OnboardComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'sign-in', component: SignInComponent},
 ];
 
 @NgModule({
