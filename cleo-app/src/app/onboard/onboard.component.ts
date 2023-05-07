b629e571c1ad4000e0fb980581ae679e4bffdf71
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 08:54:36 +0200

    onboard: component generated

---
+++ cleo-app/src/app/onboard/onboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-onboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './onboard.component.html',
+  styleUrls: ['./onboard.component.scss']
+})
+export class OnboardComponent {
+
+}
