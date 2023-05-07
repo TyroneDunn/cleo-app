01e381d295c6dc05b8e2a1820a8f160631c03905
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-07 09:26:19 +0200

    about: drafted template

--- cleo-app/src/app/about/about.component.ts
+++ cleo-app/src/app/about/about.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
