d9833703fdc2e29a09094110d4a3a4a59c827ead
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-08 21:10:44 +0200

    onboard: implement authorized state and check

--- cleo-app/src/app/onboard/onboard.component.ts
+++ cleo-app/src/app/onboard/onboard.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import {UserService} from "../user/user.service";
+import {SubSink} from "../../utils/sub-sink";
+import {BehaviorSubject, delay, timeout} from "rxjs";
+
+type State = 'loading' | 'isAuthorized' | 'notAuthorized';
 
 @Component({
   selector: 'app-onboard',
@@ -19,5 +24,25 @@ import {RouterLink} from "@angular/router";
   styleUrls: ['./onboard.component.scss']
 })
 export class OnboardComponent {
-  // public user = inject(UserService);
+  public userService = inject(UserService);
+  private sink = new SubSink();
+  public authorized$ = new BehaviorSubject<State>("loading");
+
+  public ngOnInit() {
+    this.sink.collect(
+      this.userService.authorized$
+        .pipe(
+          timeout(2000),
+          delay(2000),
+        )
+        .subscribe((okStatus) => {
+          if (!okStatus) {
+            this.authorized$.next("notAuthorized");
+            return;
+          }
+
+          this.authorized$.next("isAuthorized");
+      })
+    );
+  }
 }
