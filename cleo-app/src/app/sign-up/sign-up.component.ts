e551d6ef7f83a74aef21cb72d811025df20321c3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-08 18:00:24 +0200

    sign-up: implemented sign up

--- cleo-app/src/app/sign-up/sign-up.component.ts
+++ cleo-app/src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
@@ -9,6 +9,9 @@ import {BehaviorSubject} from "rxjs";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators}
   from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
+import {UserService} from "../user/user.service";
+import {SubSink} from "../../utils/sub-sink";
+import {HOME, JOURNALS} from "../app-routing.constants";
 
 @Component({
   selector: 'app-sign-up',
@@ -28,11 +31,14 @@ import {MatInputModule} from "@angular/material/input";
 })
 export class SignUpComponent {
   private formBuilder = inject(FormBuilder);
-  public error = new BehaviorSubject<string>('');
+  private router = inject(Router);
+  private userService = inject(UserService);
+  private sink = new SubSink();
   public signUpForm: FormGroup = this.formBuilder.group({
     username: ['', Validators.minLength(4)],
     password: ['', Validators.minLength(8)],
   })
+  public error = new BehaviorSubject<string>('');
 
   public submit() {
     if (this.signUpForm.get('username')?.invalid
@@ -50,9 +56,31 @@ export class SignUpComponent {
       this.error.next('Invalid password.');
       return;
     }
+
+    const username = this.signUpForm.get('username')?.value as string;
+    const password = this.signUpForm.get('password')?.value as string;
+
+    this.sink.collect(
+      this.userService.register$(username, password).subscribe(async (status) => {
+        if (!status) {
+          this.error.next('Username already exists.');
+          return;
+        }
+
+        this.userService.login$(username, password).subscribe(async (status) => {
+          if (!status) {
+            await this.router.navigate([HOME]);
+            return;
+          }
+
+          await this.router.navigate([JOURNALS]);
+        })
+      })
+    );
   }
 
   ngOnDestroy() {
     this.error.unsubscribe();
+    this.sink.drain();
   }
 }
