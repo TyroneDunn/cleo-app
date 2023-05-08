d01b70ccbe2d23b3df42cdd0a35059fecdede5e7
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-08 18:12:26 +0200

    app-routing: added journals route

--- cleo-app/src/app/app-routing.module.ts
+++ cleo-app/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {OnboardComponent} from "./onboard/onboard.component";
 import {AboutComponent} from "./about/about.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {JournalsComponent} from "./journal/journals/journals.component";
 
 const routes: Routes = [
   {path: '', component: OnboardComponent},
   {path: 'about', component: AboutComponent},
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
+  {path: 'journals', component: JournalsComponent},
 ];
 
 @NgModule({
