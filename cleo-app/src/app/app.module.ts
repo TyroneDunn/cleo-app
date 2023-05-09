ecfc6c78229826eda3b199ff5aa7cab25e2fbab3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 12:49:24 +0200

    app: added toolbar to component

--- cleo-app/src/app/app.module.ts
+++ cleo-app/src/app/app.module.ts
@@ -4,17 +4,19 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
+import {ToolbarComponent} from "./toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ToolbarComponent,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
