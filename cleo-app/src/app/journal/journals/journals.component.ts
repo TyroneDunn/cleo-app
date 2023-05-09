8e277672cddf23122ebffde519cd6a27ee526e5d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 13:08:20 +0200

    journals: added loading progress bar

--- cleo-app/src/app/journal/journals/journals.component.ts
+++ cleo-app/src/app/journal/journals/journals.component.ts
@@ -2,11 +2,15 @@ import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Observable} from "rxjs";
 import {Journal} from "../journal.type";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 @Component({
   selector: 'app-journals',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatProgressBarModule
+  ],
   templateUrl: './journals.component.html',
   styleUrls: ['./journals.component.scss']
 })
