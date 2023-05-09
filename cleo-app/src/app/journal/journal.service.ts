46b85c51443f7284091c208e39bdfdbb29f6b915
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 15:39:57 +0200

    journal-service: implemented journals$

--- cleo-app/src/app/journal/journal.service.ts
+++ cleo-app/src/app/journal/journal.service.ts
@@ -1,9 +1,23 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpService} from "../http/http.service";
+import {catchError, map, Observable, of} from "rxjs";
+import {Journal} from "./journal.type";
+import {CLEO_API_JOURNALS_URL} from "./journal.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JournalService {
   private http = inject(HttpService);
+
+  public journals$(): Observable<Journal[]> {
+    return this.http.getRequest$<Journal[]>(CLEO_API_JOURNALS_URL).pipe(
+      map((response) => {
+        return response.body as Journal[];
+      }),
+      catchError(() => {
+        return of([]);
+      }),
+    );
+  };
 }
