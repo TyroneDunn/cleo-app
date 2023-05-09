3e9c2d55fb64790d2b98f51aab0a5dfeca45e3ca
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 16:47:35 +0200

    create-journal: generated component

---
+++ cleo-app/src/app/journal/create-journal/create-journal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-journal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-journal.component.html',
+  styleUrls: ['./create-journal.component.scss']
+})
+export class CreateJournalComponent {
+
+}
