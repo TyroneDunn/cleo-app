b16a147d5313243176ff58a14bfcfe51097804da
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 15:58:26 +0200

    user-constants: refactor url

--- cleo-app/src/app/user/user.constants.ts
+++ cleo-app/src/app/user/user.constants.ts
@@ -1,4 +1,5 @@
-export const CLEO_API_URL = 'http://localhost:8240/'
+import {CLEO_API_URL} from "../cleo-api/cleo-api.constants";
+
 const CLEO_API_AUTH_URL = CLEO_API_URL + 'auth/';
 export const CLEO_API_PROTECTED_URL = CLEO_API_AUTH_URL + 'protected/';
 export const CLEO_API_LOGIN_URL = CLEO_API_AUTH_URL + 'login/';
