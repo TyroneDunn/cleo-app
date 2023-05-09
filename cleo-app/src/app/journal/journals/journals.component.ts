8c1d77dcfdbbcbd54a35d5aa385eb8bb9858247b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 13:23:37 +0200

    journals: added journals grid list

--- cleo-app/src/app/journal/journals/journals.component.ts
+++ cleo-app/src/app/journal/journals/journals.component.ts
@@ -1,15 +1,19 @@
 import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Observable} from "rxjs";
 import {Journal} from "../journal.type";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-journals',
   standalone: true,
   imports: [
     CommonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatGridListModule,
+    MatListModule
   ],
   templateUrl: './journals.component.html',
   styleUrls: ['./journals.component.scss']
