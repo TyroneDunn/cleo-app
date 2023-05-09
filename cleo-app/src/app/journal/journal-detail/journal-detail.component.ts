d8c26455d8670cfe46dcb02b062013f3a6de373c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 16:52:05 +0200

    journal-detail: generated component

---
+++ cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-journal-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './journal-detail.component.html',
+  styleUrls: ['./journal-detail.component.scss']
+})
+export class JournalDetailComponent {
+
+}
