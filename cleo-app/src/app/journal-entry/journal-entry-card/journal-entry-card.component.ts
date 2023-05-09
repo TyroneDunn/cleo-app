2879800135145b4536ce3f7d20fd5bf5ce660a0e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 22:57:46 +0200

    journal-entry-card: generated component

---
+++ cleo-app/src/app/journal-entry/journal-entry-card/journal-entry-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-journal-entry-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './journal-entry-card.component.html',
+  styleUrls: ['./journal-entry-card.component.scss']
+})
+export class JournalEntryCardComponent {
+
+}
