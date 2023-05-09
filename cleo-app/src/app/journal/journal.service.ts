8e8507990a1febb98c931a703d84fa784489a5b7
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 13:28:51 +0200

    journal-service: injected http service

--- cleo-app/src/app/journal/journal.service.ts
+++ cleo-app/src/app/journal/journal.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpService} from "../http/http.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JournalService {
+  private http = inject(HttpService);
 }
