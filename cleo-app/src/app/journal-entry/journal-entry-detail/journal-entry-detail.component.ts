d251405fbdf42432ef7a215b75837dfd7f22343d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-09 16:56:06 +0200

    journal-entry-detail: generated component

---
+++ cleo-app/src/app/journal-entry/journal-entry-detail/journal-entry-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-journal-entry-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './journal-entry-detail.component.html',
+  styleUrls: ['./journal-entry-detail.component.scss']
+})
+export class JournalEntryDetailComponent {
+
+}
