af7c8e4bd63631a637093002f7df0277a02f7b85
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 13:57:12 +0200

    journal-detail: implement journal data retrieval using route params

--- cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
+++ cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
@@ -1,6 +1,17 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Journal} from "../journal.type";
+import {JournalService} from "../journal.service";
+import {ActivatedRoute} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
+import {SubSink} from "../../../utils/sub-sink";
+
+const CACHED_JOURNAL: Journal = {
+  _id: '0',
+  name: '',
+  lastUpdated: new Date(),
+  dateOfCreation: new Date(),
+}
 
 @Component({
   selector: 'app-journal-detail',
@@ -10,5 +21,29 @@ import {Journal} from "../journal.type";
   styleUrls: ['./journal-detail.component.scss']
 })
 export class JournalDetailComponent {
-@Input() journal!: Journal;
+  private route = inject(ActivatedRoute);
+  private journalService = inject(JournalService);
+  private id = new BehaviorSubject<string>('');
+  private journal = new BehaviorSubject<Journal>(CACHED_JOURNAL);
+  private sink = new SubSink();
+
+  public ngOnInit() {
+    this.sink.collect(
+      this.route.paramMap.subscribe(params => {
+        const id = params.get('id') as string;
+        this.id.next(id);
+        this.sink.collect(
+          this.journalService.journal$(this.id.value).subscribe((journal) => {
+            this.journal.next(journal);
+          })
+        )
+      })
+    );
+  }
+
+  public ngOnDestroy() {
+    this.sink.drain();
+    this.id.unsubscribe();
+    this.journal.unsubscribe();
+  }
 }
