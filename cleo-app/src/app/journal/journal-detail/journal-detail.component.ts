766447c8465c4e10700c7b828f6411b53d5e4e3f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 14:09:19 +0200

    journal-detail: injected journal service

--- cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
+++ cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {JournalService} from "../journal.service";
 
 @Component({
   selector: 'app-journal-detail',
@@ -9,4 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./journal-detail.component.scss']
 })
 export class JournalDetailComponent {
+  private journalService = inject(JournalService);
 }
