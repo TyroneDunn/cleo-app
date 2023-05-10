eddb774efb5cb08d23f9513ec9c5b23ba9010869
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 20:10:04 +0200

    journal-entry-detail: defined template toolbar

--- cleo-app/src/app/journal-entry/journal-entry-detail/journal-entry-detail.component.ts
+++ cleo-app/src/app/journal-entry/journal-entry-detail/journal-entry-detail.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-journal-entry-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule, RouterLink],
   templateUrl: './journal-entry-detail.component.html',
   styleUrls: ['./journal-entry-detail.component.scss']
 })
 export class JournalEntryDetailComponent {
 
+  navigateBack() {
+  }
 }
