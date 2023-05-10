3243589bc4b48e048a7dacecc563fbc2469862a2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 14:12:59 +0200

    journal-detail: declared id & journal subjects

--- cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
+++ cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
@@ -2,6 +2,15 @@ import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {JournalService} from "../journal.service";
 import {ActivatedRoute} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
+import {Journal} from "../journal.type";
+
+const CACHED_JOURNAL: Journal = {
+  _id: '',
+  name: '',
+  lastUpdated: new Date(),
+  dateOfCreation: new Date(),
+}
 
 @Component({
   selector: 'app-journal-detail',
@@ -13,4 +22,11 @@ import {ActivatedRoute} from "@angular/router";
 export class JournalDetailComponent {
   private journalService = inject(JournalService);
   private route = inject(ActivatedRoute);
+  private id = new BehaviorSubject<string>('');
+  private journal = new BehaviorSubject<Journal>(CACHED_JOURNAL);
+
+  public ngOnDestroy() {
+    this.id.unsubscribe();
+    this.journal.unsubscribe();
+  }
 }
