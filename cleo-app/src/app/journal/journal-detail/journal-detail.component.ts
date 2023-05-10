cd4221c91cdc044f897e51820f5e99c37213cb0a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 14:09:52 +0200

    journal-detail: injected activated route

--- cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
+++ cleo-app/src/app/journal/journal-detail/journal-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {JournalService} from "../journal.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-journal-detail',
@@ -11,4 +12,5 @@ import {JournalService} from "../journal.service";
 })
 export class JournalDetailComponent {
   private journalService = inject(JournalService);
+  private route = inject(ActivatedRoute);
 }
