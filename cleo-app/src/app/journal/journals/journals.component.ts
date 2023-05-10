3ac1e499c62eb7b8648234407d7e39ef35a39d25
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 11:39:53 +0200

    journals: implemented new journal method

--- cleo-app/src/app/journal/journals/journals.component.ts
+++ cleo-app/src/app/journal/journals/journals.component.ts
@@ -6,13 +6,16 @@ import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatListModule} from "@angular/material/list";
 import {JournalService} from "../journal.service";
-import {JournalDetailComponent} from "../journal-detail/journal-detail.component";
-import {
-  JournalEntryDetailComponent
-} from "../../journal-entry/journal-entry-detail/journal-entry-detail.component";
+import {JournalDetailComponent}
+  from "../journal-detail/journal-detail.component";
+import {JournalEntryDetailComponent}
+  from "../../journal-entry/journal-entry-detail/journal-entry-detail.component";
 import {JournalCardComponent} from "../journal-card/journal-card.component";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {MatButtonModule} from "@angular/material/button";
+import {SubSink} from "../../../utils/sub-sink";
+
+const NEW_JOURNAL_PLACEHOLDER = 'New Journal';
 
 @Component({
   selector: 'app-journals',
@@ -33,9 +36,25 @@ import {MatButtonModule} from "@angular/material/button";
 })
 export class JournalsComponent {
   private journalsService = inject(JournalService);
+  private router = inject(Router);
+  private sink = new SubSink();
   public journals$!: Observable<Journal[]>;
 
   public ngOnInit() {
     this.journals$ = this.journalsService.journals$();
+  }
+
+  public newJournal() {
+    this.sink.collect(
+      this.journalsService.createJournal$(NEW_JOURNAL_PLACEHOLDER)
+        .subscribe(async (journal) => {
+          if (!journal) return;
+          await this.router.navigate([`journal/${journal.id}`]);
+        })
+    );
+  }
+
+  public ngOnDestroy() {
+    this.sink.drain();
   }
 }
