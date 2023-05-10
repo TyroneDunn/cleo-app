35ba3c8c163f75d33c20aef87d1aa1c192bf902f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 14:30:15 +0200

    journal-card: implemented router link

--- cleo-app/src/app/journal/journal-card/journal-card.component.ts
+++ cleo-app/src/app/journal/journal-card/journal-card.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Journal} from "../journal.type";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-journal-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './journal-card.component.html',
   styleUrls: ['./journal-card.component.scss']
 })
