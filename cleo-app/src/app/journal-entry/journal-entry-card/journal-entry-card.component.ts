35d2c651c768e4645424ff224accbae18f370109
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-10 16:40:38 +0200

    journal-entry-card.component: added router link to journal entry detail

--- cleo-app/src/app/journal-entry/journal-entry-card/journal-entry-card.component.ts
+++ cleo-app/src/app/journal-entry/journal-entry-card/journal-entry-card.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {JournalEntry} from "../journal-entry.type";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-journal-entry-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './journal-entry-card.component.html',
   styleUrls: ['./journal-entry-card.component.scss']
 })
