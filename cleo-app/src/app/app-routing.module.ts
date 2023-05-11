4069a61cab02e21d4b823a54c44a41811136ac4d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-11 21:36:32 +0200

    routing: defined home route

--- cleo-app/src/app/app-routing.module.ts
+++ cleo-app/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {OnboardComponent} from "./onboard/onboard.component";
 import {AboutComponent} from "./about/about.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {JournalsComponent} from "./journal/journals/journals.component";
 import {JournalDetailComponent} from "./journal/journal-detail/journal-detail.component";
-import {
-  JournalEntryDetailComponent
-} from "./journal-entry/journal-entry-detail/journal-entry-detail.component";
+import {JournalEntryDetailComponent}
+  from "./journal-entry/journal-entry-detail/journal-entry-detail.component";
 
 const routes: Routes = [
   {path: '', component: OnboardComponent},
+  {path: 'home', component: OnboardComponent},
   {path: 'about', component: AboutComponent},
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
