9290a039ef3849fde1064dca8341b01b4f5ef09c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-11 21:55:48 +0200

    auth-check: moved logic from onboard to app

--- cleo-app/src/app/app-routing.constants.ts
+++ cleo-app/src/app/app-routing.constants.ts
@@ -1,3 +1,3 @@
-export const HOME = '';
+export const HOME = 'home/';
 export const SIGN_IN = 'sign-in/';
 export const JOURNALS = 'journals/';
--- cleo-app/src/app/app.module.ts
+++ cleo-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
+import {SplashComponent} from "./splash/splash.component";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import {HttpClientModule} from "@angular/common/http";
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
+        SplashComponent,
     ],
   providers: [],
   bootstrap: [AppComponent]
