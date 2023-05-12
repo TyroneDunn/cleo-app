48aef790290d9cfd8230e8c67b8c8786e3df6013
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 17:01:59 +0200

    journal-card: implemented delete journal

--- cleo-app/src/app/journal/journal-card/journal-card.component.ts
+++ cleo-app/src/app/journal/journal-card/journal-card.component.ts
@@ -1,11 +1,15 @@
-import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {Journal} from "../journal.type";
 import {RouterLink} from "@angular/router";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
+import {MatDialog} from "@angular/material/dialog";
+import {DeleteJournalComponent} from "../delete-journal/delete-journal.component";
+import {JournalService} from "../journal.service";
+import {SubSink} from "../../../utils/sub-sink";
 
 @Component({
   selector: 'app-journal-card',
@@ -22,5 +26,30 @@ import {MatMenuModule} from "@angular/material/menu";
   styleUrls: ['./journal-card.component.scss']
 })
 export class JournalCardComponent {
+  public dialog = inject(MatDialog);
+  private journalService = inject(JournalService);
+  private sink = new SubSink();
   @Input() journal!: Journal;
+  @Output() onDeleteJournal = new EventEmitter<Journal>();
+
+  public openDeleteJournalDialog() {
+    const dialogRef = this.dialog.open(DeleteJournalComponent, {
+      data: {
+        journal: this.journal,
+        confirm: false,
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirm: boolean) => {
+      console.log('Dialog result: ', confirm);
+      if (confirm) {
+        this.sink.collect(
+          this.journalService.deleteJournal$(this.journal._id).subscribe((success) => {
+            if (success)
+              this.onDeleteJournal.emit(this.journal);
+          })
+        );
+      }
+    });
+  }
 }
