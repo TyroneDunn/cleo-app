0bebc38a56cf3cf02ef51a7589103b4c2c4a2a4d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 17:44:49 +0200

    edit-journal: generated component

---
+++ cleo-app/src/app/journal/edit-journal/edit-journal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-journal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edit-journal.component.html',
+  styleUrls: ['./edit-journal.component.scss']
+})
+export class EditJournalComponent {
+
+}
