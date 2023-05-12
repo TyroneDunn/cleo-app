1685bca46199153a754864d5d85f8ae53b0de1db
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 11:40:36 +0200

    onboard: handled authorized user options

--- cleo-app/src/app/onboard/onboard.component.ts
+++ cleo-app/src/app/onboard/onboard.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {SplashComponent} from "../splash/splash.component";
+import {UserService} from "../user/user.service";
 
 @Component({
   selector: 'app-onboard',
@@ -20,4 +21,7 @@ import {SplashComponent} from "../splash/splash.component";
   templateUrl: './onboard.component.html',
   styleUrls: ['./onboard.component.scss']
 })
-export class OnboardComponent {}
+export class OnboardComponent {
+  private userService = inject(UserService);
+  public authorized$ = this.userService.authorized$;
+}
