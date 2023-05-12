19ddc6f408f62beced53990b306ffe8fa25e6caf
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 11:25:20 +0200

    about: refactor navigation

--- cleo-app/src/app/about/about.component.ts
+++ cleo-app/src/app/about/about.component.ts
@@ -4,17 +4,19 @@ import {RouterLink} from "@angular/router";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterLink,
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule,
+        MatToolbarModule,
+    ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
