9907345d918711e4efeb9a340f2b6e3e637d60f9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 10:37:11 +0200

    new-journal: generated component

---
+++ cleo-app/src/app/journal/new-journal/new-journal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-journal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-journal.component.html',
+  styleUrls: ['./new-journal.component.scss']
+})
+export class NewJournalComponent {
+
+}
