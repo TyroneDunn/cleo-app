8a58c152d5ca95d88b4a2a94f53d89e771c893e9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-12 14:42:00 +0200

    journal-card: developed template

--- cleo-app/src/app/journal/journal-card/journal-card.component.ts
+++ cleo-app/src/app/journal/journal-card/journal-card.component.ts
@@ -2,11 +2,19 @@ import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Journal} from "../journal.type";
 import {RouterLink} from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-journal-card',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './journal-card.component.html',
   styleUrls: ['./journal-card.component.scss']
 })
