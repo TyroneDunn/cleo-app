beb7a205a202bb1972f69dbe89215317d4d57967
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-10-25 11:35:34 +0200

    feat: define get, create, update entry DTOs

--- cleo-app/src/app/journal-entry/entry-dtos.ts
+++ cleo-app/src/app/journal-entry/entry-dtos.ts
@@ -1,8 +1,30 @@
 import {Entry} from "./entry.type";
 
+export type GetEntriesDTO = {
+  bodyRegex?: string,
+  journal?: string,
+  startDate?: string,
+  endDate?: string,
+  sort?: 'body' | 'dateCreated' | 'lastUpdated',
+  order?: 1 | -1,
+  page?: number,
+  limit?: number,
+};
+
 export type GetEntriesResponseDTO = {
   count: number,
   entries: Entry[],
   page?: number,
   limit?: number,
+};
+
+export type CreateEntryDTO = {
+  journal: string,
+  body: string,
+};
+
+export type UpdateEntryDTO = {
+  id: string,
+  body?: string,
+  journal?: string,
 };
